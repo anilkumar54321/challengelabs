9f69dbd112efba7077c32a58a2d63a9f2392fc92
Anil Marupaka <92710860+anilkumar54321@users.noreply.github.com>
2025-12-17 21:52:06 +0530

    Update main.tf

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -1,5 +1,20 @@
 resource "google_project_service" "api_services" {
-  project            = "project-f8951ee2-0f63-4239-9c6"
-  service            = "sheets.googleapis.com"
-  disable_on_destroy = true
+  project            = var.project_id
+  service            = var.api_service
+  disable_on_destroy = var.disable_on_destroy
+}
+
+variable "project_id" {
+  description = "The ID of the Google Cloud project where the API will be enabled."
+  type        = string
+}
+
+variable "api_service" {
+  description = "The API service name to enable (e.g. sheets.googleapis.com)."
+  type        = string
+}
+
+variable "disable_on_destroy" {
+  description = "Whether to disable the API when this resource is destroyed."
+  type        = bool
 }
