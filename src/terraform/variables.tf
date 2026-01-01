c2a24ee3bf61b42bea7dbb94b030ef31b1da3585
Anil Marupaka <92710860+anilkumar54321@users.noreply.github.com>
2026-01-01 16:35:58 +0530

    Update variables.tf

--- src/terraform/variables.tf
+++ src/terraform/variables.tf
@@ -1,3 +1,13 @@
+## common inputs ##
+variable "main" {
+  type = object({
+    project = string
+    region  = string
+    zone    = string
+  })
+}
+
+## service account ##
 variable "service_accounts" {
   type = map(object({
     account_id   = string
