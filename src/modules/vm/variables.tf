59f89d581a35c37e1eb2030da1c58db141c89872
Anil <marupakaanil.kumar@infosys.com>
2026-01-05 15:57:41 +0000

    module structure

---
+++ src/modules/vm/variables.tf
@@ -0,0 +1,34 @@
+## VM Variables ##
+variable "name" {
+  type = string
+}
+variable "machine_type" {
+  type = string
+}
+variable "image" {
+  type = string
+}
+variable "size" {
+  type = number
+}
+variable "labels" {
+  type = map(string)
+}
+variable "type" {
+  type = string
+}
+variable "allow_stopping_for_update" {
+  type = bool
+}
+variable "network" {
+  type = string
+}
+variable "subnetwork" {
+  type = string
+}
+variable "tags" {
+  type = list(string)
+}
+variable "zone" {
+  type = string
+}
---
+++ src/modules/vm/vm.tf
@@ -0,0 +1,20 @@
+resource "google_compute_instance" "vminstance" {
+  name                = var.name
+  machine_type        = var.machine_type
+  zone                = var.zone
+  deletion_protection = false
+  tags                = var.tags
+  boot_disk {
+    initialize_params {
+      image  = var.image
+      size   = var.size
+      type   = var.type
+      labels = var.labels
+    }
+  }
+  network_interface {
+    network    = var.network
+    subnetwork = var.subnetwork
+  }
+  allow_stopping_for_update = var.allow_stopping_for_update
+}
