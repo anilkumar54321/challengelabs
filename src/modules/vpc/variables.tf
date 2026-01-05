da010c6929008e781b68422e9f9be0d106ff424b
Anil <marupakaanil.kumar@infosys.com>
2026-01-05 15:59:59 +0000

    module structure

---
+++ src/modules/vpc/variables.tf
@@ -0,0 +1,17 @@
+variable "project" {
+  type = string
+}
+variable "network_name" {
+  type = string
+}
+variable "routing_mode" {
+  type = string
+}
+variable "description" {
+  type = string
+}
+variable "auto_create_subnetworks" {
+  type = bool
+}
+
+
